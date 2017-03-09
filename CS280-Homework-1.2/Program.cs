1d439e5831ee07d82fd7ed4fd2addc78ea0a432f
menghh1221 <m9108018@chu.edu.tw>
2017-03-09 21:38:29 +0800

    初始化專案

---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Ability = "Blaze";
+            pokemon.Catagory = "Lizard";
+            pokemon.Gender = "M";
+            pokemon.Number = 1f;
+            pokemon.Name = "Pokemon Number1";
+            Console.WriteLine("身高；{0}公尺，體重：{1}磅，姓名：{2}，編號：{3}", pokemon.Height, pokemon.Weight, pokemon.Name, pokemon.Number);
+        }
+    }
+}
